moto-sm7250/android_device_motorola_kiev

kiev: boardconfig.mk: prepare to build kernel with twrp
Signed-off-by: Andrew Hexen <SyberHexen@gmail.com>
Change-Id: I5f4067c55e451d58618d55e2278263e567263f87

BoardConfig.mk
@@ -57,18 +57,19 @@ TARGET_USES_HARDWARE_QCOM_BOOTCTRL := true
 BOARD_PROVIDES_GPTUTILS := true
 
 # Kernel
-BOARD_KERNEL_CMDLINE := \
-    console=ttyMSM0,115200,n8 \
-    androidboot.hardware=qcom \
-    androidboot.console=ttyMSM0 \
+BOARD_BOOT_HEADER_VERSION := 2
+BOARD_KERNEL_BASE := 0x00000000
+BOARD_KERNEL_CMDLINE += \
+    console=ttyMSM0,115200,n8  \
+    earlycon=msm_geni_serial,0x888000 \
+    androidboot.hardware=qcom androidboot.console=ttyMSM0 \
     androidboot.memcg=1 \
-    androidboot.usbcontroller=a600000.dwc3 \
     lpm_levels.sleep_disabled=1 \
     video=vfb:640x400,bpp=32,memsize=3072000 \
-    msm_rtb.filter=0x237 \
+    msm_rtb.filter=0x237  \
     service_locator.enable=1 \
+    androidboot.usbcontroller=a600000.dwc3 \
     swiotlb=2048 \
-    earlycon=msm_geni_serial,0x888000 \
     loop.max_part=7 \
     cgroup.memory=nokmem,nosocket \
     androidboot.boot_devices=soc/1d84000.ufshc \
@@ -77,38 +78,17 @@ BOARD_KERNEL_CMDLINE := \
 BOARD_KERNEL_CMDLINE += androidboot.fastboot=1
 BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 
-BOARD_BOOT_HEADER_VERSION := 2
-BOARD_KERNEL_PAGESIZE := 4096
-BOARD_KERNEL_BASE          := 0x00000000
-BOARD_KERNEL_OFFSET        := 0x00008000
-BOARD_RAMDISK_OFFSET       := 0x01000000
-BOARD_KERNEL_SECOND_OFFSET := 0x00f00000
-BOARD_KERNEL_TAGS_OFFSET   := 0x00000100
-BOARD_DTB_OFFSET           := 0x01f00000
-
 BOARD_INCLUDE_DTB_IN_BOOTIMG := true
-
-TARGET_PREBUILT_DTB := $(DEVICE_PATH)/prebuilt/dtb.img
-TARGET_PREBUILT_KERNEL := $(DEVICE_PATH)/prebuilt/Image.gz
-BOARD_PREBUILT_DTBOIMAGE := $(DEVICE_PATH)/prebuilt/dtbo.img
-BOARD_KERNEL_IMAGE_NAME := Image.gz
+BOARD_KERNEL_IMAGE_NAME := Image
+BOARD_KERNEL_PAGESIZE := 4096
+BOARD_KERNEL_SEPARATED_DTBO := true
+BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOT_HEADER_VERSION)
 
 TARGET_KERNEL_VERSION := 4.19
 TARGET_KERNEL_ARCH := arm64
-TARGET_KERNEL_HEADER_ARCH := arm64
-
 TARGET_KERNEL_CLANG_COMPILE := true
-TARGET_KERNEL_CLANG_VERSION := r328903
-
-BOARD_MKBOOTIMG_ARGS += --base $(BOARD_KERNEL_BASE)
-BOARD_MKBOOTIMG_ARGS += --pagesize $(BOARD_KERNEL_PAGESIZE)
-BOARD_MKBOOTIMG_ARGS += --ramdisk_offset $(BOARD_RAMDISK_OFFSET)
-BOARD_MKBOOTIMG_ARGS += --tags_offset $(BOARD_KERNEL_TAGS_OFFSET)
-BOARD_MKBOOTIMG_ARGS += --kernel_offset $(BOARD_KERNEL_OFFSET)
-BOARD_MKBOOTIMG_ARGS += --second_offset $(BOARD_KERNEL_SECOND_OFFSET)
-BOARD_MKBOOTIMG_ARGS += --dtb_offset $(BOARD_DTB_OFFSET)
-BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOT_HEADER_VERSION)
-BOARD_MKBOOTIMG_ARGS += --dtb $(TARGET_PREBUILT_DTB)
+TARGET_KERNEL_CONFIG := vendor/kiev_recovery_defconfig
+TARGET_KERNEL_SOURCE := kernel/motorola/sm7250
 
 # Partitions
 BOARD_BOOTIMAGE_PARTITION_SIZE := 100663296     #    98304

SyberHexen/android_device_motorola_nairo

BoardConfig.mk
@@ -57,18 +57,19 @@ TARGET_USES_HARDWARE_QCOM_BOOTCTRL := true
 BOARD_PROVIDES_GPTUTILS := true
 
 # Kernel
-BOARD_KERNEL_CMDLINE := \
-    console=ttyMSM0,115200,n8 \
-    androidboot.hardware=qcom \
-    androidboot.console=ttyMSM0 \
+BOARD_BOOT_HEADER_VERSION := 2
+BOARD_KERNEL_BASE := 0x00000000
+BOARD_KERNEL_CMDLINE += \
+    console=ttyMSM0,115200,n8  \
+    earlycon=msm_geni_serial,0x888000 \
+    androidboot.hardware=qcom androidboot.console=ttyMSM0 \
     androidboot.memcg=1 \
-    androidboot.usbcontroller=a600000.dwc3 \
     lpm_levels.sleep_disabled=1 \
     video=vfb:640x400,bpp=32,memsize=3072000 \
-    msm_rtb.filter=0x237 \
+    msm_rtb.filter=0x237  \
     service_locator.enable=1 \
+    androidboot.usbcontroller=a600000.dwc3 \
     swiotlb=2048 \
-    earlycon=msm_geni_serial,0x888000 \
     loop.max_part=7 \
     cgroup.memory=nokmem,nosocket \
     androidboot.boot_devices=soc/1d84000.ufshc \
@@ -77,38 +78,17 @@ BOARD_KERNEL_CMDLINE := \
 BOARD_KERNEL_CMDLINE += androidboot.fastboot=1
 BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 
-BOARD_BOOT_HEADER_VERSION := 2
-BOARD_KERNEL_PAGESIZE := 4096
-BOARD_KERNEL_BASE          := 0x00000000
-BOARD_KERNEL_OFFSET        := 0x00008000
-BOARD_RAMDISK_OFFSET       := 0x01000000
-BOARD_KERNEL_SECOND_OFFSET := 0x00f00000
-BOARD_KERNEL_TAGS_OFFSET   := 0x00000100
-BOARD_DTB_OFFSET           := 0x01f00000
-
 BOARD_INCLUDE_DTB_IN_BOOTIMG := true
-
-TARGET_PREBUILT_DTB := $(DEVICE_PATH)/prebuilt/dtb.img
-TARGET_PREBUILT_KERNEL := $(DEVICE_PATH)/prebuilt/Image.gz
-BOARD_PREBUILT_DTBOIMAGE := $(DEVICE_PATH)/prebuilt/dtbo.img
-BOARD_KERNEL_IMAGE_NAME := Image.gz
+BOARD_KERNEL_IMAGE_NAME := Image
+BOARD_KERNEL_PAGESIZE := 4096
+BOARD_KERNEL_SEPARATED_DTBO := true
+BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOT_HEADER_VERSION)
 
 TARGET_KERNEL_VERSION := 4.19
 TARGET_KERNEL_ARCH := arm64
-TARGET_KERNEL_HEADER_ARCH := arm64
-
 TARGET_KERNEL_CLANG_COMPILE := true
-TARGET_KERNEL_CLANG_VERSION := r328903
-
-BOARD_MKBOOTIMG_ARGS += --base $(BOARD_KERNEL_BASE)
-BOARD_MKBOOTIMG_ARGS += --pagesize $(BOARD_KERNEL_PAGESIZE)
-BOARD_MKBOOTIMG_ARGS += --ramdisk_offset $(BOARD_RAMDISK_OFFSET)
-BOARD_MKBOOTIMG_ARGS += --tags_offset $(BOARD_KERNEL_TAGS_OFFSET)
-BOARD_MKBOOTIMG_ARGS += --kernel_offset $(BOARD_KERNEL_OFFSET)
-BOARD_MKBOOTIMG_ARGS += --second_offset $(BOARD_KERNEL_SECOND_OFFSET)
-BOARD_MKBOOTIMG_ARGS += --dtb_offset $(BOARD_DTB_OFFSET)
-BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOT_HEADER_VERSION)
-BOARD_MKBOOTIMG_ARGS += --dtb $(TARGET_PREBUILT_DTB)
+TARGET_KERNEL_CONFIG := vendor/kiev_defconfig
+TARGET_KERNEL_SOURCE := kernel/motorola/sm7250
 
 # Partitions
 BOARD_BOOTIMAGE_PARTITION_SIZE := 100663296     #    98304